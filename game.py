441eed2efe036ae2cd0cf72b43db16a61da5e8be
Echo-Gambino <nkb012pomm@gmail.com>
2020-11-10 15:32:39 -0700

    Made a more interactive version of the Y game

--- game.py
+++ game.py
@@ -1,5 +1,108 @@
+import re
+
 from board import Board
 from player import Player
+
+
+class Prompt:
+    def DisplayBoardState(board, player_to_move, move_num):
+        print("\n{}\n".format(board))
+        print("Move number: {}".format(move_num))
+        print("It is now {}'s turn:".format(player_to_move))
+        return
+
+    def ReportMove(board, player_to_move, move):
+        print("{} plays {}".format(player_to_move, move))
+        print("This is the result:")
+        print("\n{}\n".format(board))
+        return
+
+    def PromptAndPlayMove(board, player_to_move, move_num):
+        context = "Playing as {}:".format(player_to_move)
+        instructions = "Please input your move (2 numbers seperated by spaces)"
+        example = "Example: \'0 2\' to play on the left-most tile on row 2"
+
+        prompt = "{}\n{}\n{}".format(context, instructions, example)
+
+        point = None
+        while (point is None) and len(board.legal_moves) != 0:
+            point = Prompt.OptionPointPrompt(prompt, board)
+            if (point is None):
+                Prompt.DisplayBoardState(board, player_to_move, move_num)
+                print("\n{}\n".format("Invalid input: Please try again."))
+        
+        return point
+
+    def OptionListPrompt(prompt_text, options, q_option="Back"):
+        print(prompt_text)
+        for i in range(len(options)):
+            print("{}) {}".format(i + 1, options[i]))
+        print("q) {}".format(q_option))
+        user_input = input("> ").strip().lower()
+
+        if user_input == 'q':
+            return 0
+        
+        if not user_input.isnumeric():
+            return -1
+
+        selection = int(user_input)
+        if (selection > len(options)) or (selection <= 0):
+            return -1
+
+        return selection
+
+    def OptionPointPrompt(prompt_text, board):
+        print(prompt_text)
+        user_input = input("> ").strip().lower()
+        
+        nums = re.findall(r'\d+', user_input)
+
+        if len(nums) != 2:
+            print("Invalid input!")
+            return None
+        
+        legal_moves = board.legal_moves
+
+        pt = (int(nums[0]), int(nums[1]))
+        
+        print(pt)
+        print(legal_moves)
+
+        if not (pt in legal_moves):
+            print("Move is not legal!")
+            return None
+
+        return pt
+
+    def ShowBanner():
+        text = [' /$$     /$$                      /$$$$$$                                   ',
+                '|  $$   /$$/                     /$$__  $$                                  ',
+                ' \  $$ /$$/                     | $$  \__/  /$$$$$$  /$$$$$$/$$$$   /$$$$$$ ',
+                '  \  $$$$/         /$$$$$$      | $$ /$$$$ |____  $$| $$_  $$_  $$ /$$__  $$',
+                '   \  $$/         |______/      | $$|_  $$  /$$$$$$$| $$ \ $$ \ $$| $$$$$$$$',
+                '    | $$                        | $$  \ $$ /$$__  $$| $$ | $$ | $$| $$_____/',
+                '    | $$                        |  $$$$$$/|  $$$$$$$| $$ | $$ | $$|  $$$$$$$',
+                '    |__/                         \______/  \_______/|__/ |__/ |__/ \_______/']
+
+        print('\n'.join(text))
+        return
+
+    def WelcomePrompt():
+        prompt_text = "Welcome to Y - Game! Please select an option:"
+
+        options = [
+            "Player vs AI",
+            "AI vs AI"
+        ]
+
+        q_option = "Quit"
+
+        user_input = -2
+        while (user_input < 0):
+            user_input = Prompt.OptionListPrompt(prompt_text, options, q_option)
+
+        return user_input
 
 
 class Game:
@@ -9,16 +112,19 @@ class Game:
 
     ### Public Methods ###
     def Start(self):
-        print("1) Player vs AI\n2) AI vs AI")
-        option = input(">")
+        Prompt.ShowBanner()
+        u_input = Prompt.WelcomePrompt()
 
-        if (option == '1'):
-            # self.PlayGame(None, self.b_player)
-            print("WIP, please select option 2 next time")
-        elif (option == '2'):
+        if u_input == 0: 
+            print("Quitting.")
+            return
+        elif u_input == 1:
+            self.PlayGame(None, Player(Board.BLACK))
+        elif u_input == 2:
             self.PlayGame(Player(Board.WHITE), Player(Board.BLACK))
         else:
-            print("option unsupported, exiting...")
+            print("Error, unknown option cannot be caught.")
+            return
         
         return
 
@@ -33,45 +139,34 @@ class Game:
 
         while outcome[0] == False:
             color = player_color[moves % 2]
+            player_to_move = Board.Color2Text[color]
 
             moves += 1
 
+            # Retrieve the player from the dictionary
             cur_player = players[color]
             
-            self._DisplayTurn(self.board, color, moves)
+            # Display the board state for context
+            Prompt.DisplayBoardState(self.board, player_to_move, moves)
             
-            point_to_play = cur_player.PlayMove(self.board)
+            # Select point to be played
+            point_to_play = None
+            if (cur_player is not None):
+                point_to_play = cur_player.PlayMove(self.board)
+            else:
+                point_to_play = Prompt.PromptAndPlayMove(self.board, player_to_move, moves)
+            
+            # Color the point on the board
             self.board.ColorPoint(point_to_play, color)
 
-            self._ReportMove(self.board, color, point_to_play)
+            # Report the move
+            Prompt.ReportMove(self.board, player_to_move, point_to_play)
 
             outcome = self.board.DetectGameEnd()
 
         winner = Board.Color2Text[outcome[1]]
         self._DisplayGameOver(self.board, winner)
 
-    ### Private Methods ###
-    def _DisplayTurn(self, board, color, move_num):
-        print()
-        print(board)
-        print()
-        print("Move {}".format(move_num))
-        print("{} to move.".format(Board.Color2Text[color]))
-        return
-
-    def _ReportMove(self, board, color, move):
-        print("{} plays {}!".format(Board.Color2Text[color], move))
-        print()
-        print(board)
-        print()
-        return
-    
-    def _DisplayGameOver(self, board, winner):
-        print()
-        print("Game over!")
-        print("The winner is {}! Congrats!".format(winner))
-        print()
-        return
 
 if __name__ == "__main__":
     game = Game(4)
