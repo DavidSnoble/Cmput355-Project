55f05768f1b5d5f1db855b85d48525e406456d7d
Julieta <dikova@ualberta.ca>
2020-11-10 19:03:29 -0700

    added the minimax function but it's not complete, need to heuristic calculation to finish and test

--- player.py
+++ player.py
@@ -35,6 +35,42 @@ class Player:
 
         return score
 
+    def minimax(self, curr_position, b, end_node_depth, maximizing_player):
+        # tutorial from https://www.youtube.com/watch?v=l-hh51ncgDI&ab_channel=SebastianLague
+
+        # current player looks to maximize (temp heuristic -> see if win)
+        # opponent looks to minimize
+        # need to perform static heuristic to get last node values
+        if end_node_depth == 0 or self.Evaluate(b) == 1:
+            # perform heuristic on position
+            #(left, right, bottom) = b.board_edges
+            #print(left)
+            #print(right)
+            #print(bottom)
+
+            # calculate heuristic here
+            return heuristic
+
+        # get the list of legal moves after current move has been played
+        legal_moves = list(b.legal_moves)
+
+        if maximizing_player:
+            # current player
+            maxVal = -99999
+            for move in legal_moves:
+                # call minimax on child node, one up from final checked node
+                # set maximizing_player to false, as this is opponent's turn
+                # and is looking to minimize
+                cur_val = self.minimax(move, b, end_node_depth - 1, False)
+                maxVal = max(maxVal, val)
+            return maxVal
+        else:
+            # opponent
+            minVal = 99999
+            for move in legal_moves:
+                cur_val = self.minimax(move, b, end_node_depth - 1, True)
+                minVal = min(minVal, val)
+            return minVal
 
     ### Private Methods ###
 
@@ -42,4 +78,14 @@ class Player:
 
 
 if __name__ == "__main__":
+    #b = Board(4)
+
+    #w_player = Player(b.WHITE)
+    #b_player = Player(b.BLACK)
+    #w_move = RandPlayerUtils.PlayMove(w_player, b)
+    #b_move = RandPlayerUtils.PlayMove(b_player, b)
+    #print(w_move)
+    #print(b_move)
+
+    #w_move = Player.minimax(w_player, w_move, b, 3, True)
     pass
